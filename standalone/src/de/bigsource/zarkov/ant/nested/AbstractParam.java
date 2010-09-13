124fa7f94022bd6a93eb7aff902eafd26e3764e1
Alexander Funke <alexander.funke@bigsource.de>
2010-09-13 17:29:06 +0200

    symbolic links set

--- standalone/src/de/bigsource/zarkov/ant/nested/AbstractParam.java
+++
@@ -1,9 +0,0 @@
-package de.bigsource.zarkov.ant.nested;
-
-public class AbstractParam {
-
-    public boolean append(){
-	return false;
-    }
-    
-}
--- standalone/src/de/bigsource/zarkov/ant/nested/EmptyParam.java
+++
@@ -1,8 +0,0 @@
-package de.bigsource.zarkov.ant.nested;
-
-public class EmptyParam extends AbstractParam {
-
-	public String toString() {
-		return "";
-	}
-}
