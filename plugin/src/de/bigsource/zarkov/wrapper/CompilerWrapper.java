b88e7a591465785cae17370350a1493d881cbfed
Alexander Funke <alexander.funke@bigsource.de>
2010-09-13 17:14:50 +0200

    fchs and wrapper in plugin integrated

--- plugin/src/de/bigsource/zarkov/wrapper/CompilerWrapper.java
+++ plugin/src/de/bigsource/zarkov/wrapper/CompilerWrapper.java
@@ -1,19 +1,24 @@
 package de.bigsource.zarkov.wrapper;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.AbstractCollection;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Iterator;
+import java.util.Map;
 
 import de.bigsource.zarkov.base.ZarkovTask;
-import de.bigsource.zarkov.base.ZarkovUtil;
+import de.bigsource.zarkov.reader.InputReader;
 import de.bigsource.zarkov.views.ZarkovMonitor;
 
 public class CompilerWrapper
 {
 	public static ZarkovTask _task;
 	private static Process _p;
-	public static String NAME;
-	public static String WIN_SUFFIX;
+	
+	public static final int BUILDTYPE_STANDARD = 0;
+	public static final int BUILDTYPE_SUBITEMS = 1;
 	
 	public CompilerWrapper()
 	{
@@ -29,12 +34,12 @@ public class CompilerWrapper
 		try
 		{
 			
-			_p = ZarkovUtil.callCommand(ZarkovUtil.getExecutable(sdk, executable, suffix), command, ZarkovUtil.BUILDTYPE_SUBITEMS);
+			_p = callCommand(getExecutable(sdk, executable, suffix), command, BUILDTYPE_SUBITEMS);
 			
-			ZarkovUtil.logCommand(command);
+			logCommand(command);
 			
-			ZarkovUtil.addReader(_p.getErrorStream(), _task);
-			ZarkovUtil.addReader(_p.getInputStream(), _task);
+			addReader(_p.getErrorStream(), _task);
+			addReader(_p.getInputStream(), _task);
 			
 			int exitval = _p.waitFor();
 			if (exitval != 0)
@@ -54,6 +59,91 @@ public class CompilerWrapper
 			ZarkovMonitor.addMessage(executable + " was interrupted.");
 		}
 		
+	}
+	
+	public static String getExecutable(String sdk, String name, String suffix)
+	{
+		if (suffix != null)
+		{
+			String osName = System.getProperty("os.name");
+			if (osName.contains("Windows"))
+			{
+				return sdk + "\\bin\\" + name + "." + suffix;
+			}
+
+			if (osName.contains("Mac") || osName.contains("Linux"))
+			{
+				return sdk + "/bin/" + name;
+			}
+		} else
+		{
+			return name;
+		}
+		return "";
+	}
+	
+	public static Process callCommand(String executable, ArrayList<String> command, int buildtype) throws IOException
+	{
+		ArrayList<String> calls = new ArrayList<String>();
+		calls.add(executable);
+
+		switch (buildtype)
+		{
+			case BUILDTYPE_STANDARD:
+
+				break;
+			case BUILDTYPE_SUBITEMS:
+				for (int i = 0; i < command.size(); i++)
+				{
+					String[] suboption = (String[]) command.get(i).split(" ");
+					calls.add(suboption[0]);
+					if (suboption.length > 2 && suboption[1] != null)
+					{
+						calls.add(suboption[1]);
+						calls.add(suboption[2]);
+					} else if (suboption.length > 1 && suboption[1] != null)
+					{
+						calls.add(suboption[1]);
+					}
+				}
+				break;
+			default:
+				break;
+		}
+
+		ProcessBuilder pb = new ProcessBuilder(calls);
+		Map<String, String> env = pb.environment();
+		env.put("-Xms32m", "");
+		env.put("-Xmx256m", "");
+
+		return pb.start();
+	}
+	
+	public static void logCommand(ArrayList<String> command)
+	{
+		ZarkovMonitor.addMessage("\n\n----------- COMMAND -----------");
+		ZarkovMonitor.addMessage(join(command, " ") + "\n\n");
+		ZarkovMonitor.addMessage("----------- OUTPUT -----------");
+	}
+
+	private static String join(AbstractCollection<String> s, String delimiter)
+	{
+		if (s == null || s.isEmpty())
+			return "";
+		Iterator<String> iter = s.iterator();
+		StringBuilder builder = new StringBuilder(iter.next());
+		while (iter.hasNext())
+		{
+			builder.append(delimiter).append(iter.next());
+		}
+		return builder.toString();
+	}
+	
+	public static void addReader(InputStream stream, ZarkovTask task)
+	{
+		InputReader errReader = new InputReader(stream, task);
+		Thread errThread = new Thread(errReader);
+		errThread.start();
 	}
 	
 	public static void quit()
