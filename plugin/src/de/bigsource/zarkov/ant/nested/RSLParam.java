6c89a6566cd69f17875c7957bc4dfcff9b173bb7
Alexander Funke <alexander.funke@bigsource.de>
2010-10-20 14:35:19 +0200

    Runtime Shared Library Path changed

---
+++ plugin/src/de/bigsource/zarkov/ant/nested/RSLParam.java
@@ -0,0 +1,52 @@
+package de.bigsource.zarkov.ant.nested;
+
+import de.bigsource.zarkov.base.TranslationUtil;
+import de.bigsource.zarkov.base.ZarkovTask;
+
+public class RSLParam extends AbstractParam
+{
+	
+	public ZarkovTask task;
+	public String option = "";
+	private String _pathElement = "";
+	private String _rslUrl = "";
+	private String _policyFileUrl1 = "";
+	private String _failoverUrl = "";
+	private String _policyFileUrl2 = "";
+	
+	public String toString() {
+
+		if (_pathElement == null) {
+			task.callException(TranslationUtil.getTranslation("param.specify_a_value_for")+" " + option);
+			return "";
+		} else {
+			return _pathElement+","+_rslUrl+","+_policyFileUrl1+","+_failoverUrl+","+_policyFileUrl2;
+		}
+	}
+	
+	public void setPathElement(String pathElement)
+	{
+		_pathElement = pathElement;
+	}
+	
+	public void setRSLUrl(String rslUrl)
+	{
+		_rslUrl = rslUrl;
+	}
+	
+	public void setPolicyFileURL1(String policyFileUrl)
+	{
+		_policyFileUrl1 = policyFileUrl;
+	}
+	
+	public void setFailoverUrl(String failoverUrl)
+	{
+		_failoverUrl = failoverUrl;
+	}
+	
+	public void setPolicyFileURL2(String policyFileUrl)
+	{
+		_policyFileUrl2 = policyFileUrl;
+	}
+	
+}
